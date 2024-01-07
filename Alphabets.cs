3d2b62c4cdf436914ac5fe144475c98313905301
DaeMoon <myakinin-dmitriy77@mail.ru>
2024-01-07 07:34:37 +0300

    ROT1 Converter

---
+++ Alphabets.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.ObjectModel;
+
+namespace ROT1_Converter
+{
+    public enum Alphabets
+    {
+        None,
+        English,
+        Spanish,
+        Russian,
+        Max,
+    }
+
+    public static class AlphabetData
+    {
+        public static Alphabets GetAlphabet(string obj)
+        {
+            if (uint.TryParse(obj, out uint ui))
+            {
+                while (ui >= (uint)Alphabets.Max)
+                    ui -= (uint)Alphabets.Max - 1;
+                return (Alphabets)ui;
+            }
+            else
+            {
+                if (Enum.TryParse(obj, true, out Alphabets res))
+                    return res;
+            }
+
+            return Alphabets.None;
+        }
+
+        public static ReadOnlyCollection<char> GetAlphabetSymbols(Alphabets alphabet)
+        {
+            switch (alphabet)
+            {
+                case Alphabets.English: return English;
+                case Alphabets.Spanish: return Spanish;
+                case Alphabets.Russian: return Russian;
+            }
+            return null;
+        }
+
+
+
+        /*
+        public static readonly ReadOnlyCollection<char> AlphabetName = new ReadOnlyCollection<char>(new char[]
+           {
+           }); 
+        */
+
+        public static readonly ReadOnlyCollection<char> Spanish = new ReadOnlyCollection<char>(new char[]
+        {
+                'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j',
+                'k', 'l', 'm', 'n', 'ñ', 'o', 'p', 'q', 'r',
+                's', 't', 'u', 'v', 'w', 'x', 'y', 'z'
+        });
+
+        public static readonly ReadOnlyCollection<char> Russian = new ReadOnlyCollection<char>(new char[]
+            {
+                'а', 'б', 'в', 'г', 'д', 'е', 'ё', 'ж', 'з', 'и', 'й',
+                'к', 'л', 'м', 'н', 'о', 'п', 'р', 'с', 'т', 'у', 'ф',
+                'х', 'ц', 'ч', 'ш', 'щ', 'ъ', 'ы', 'ь', 'э', 'ю', 'я'
+            });
+
+        public static readonly ReadOnlyCollection<char> English = new ReadOnlyCollection<char>(new char[]
+            {
+                'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h',
+                'i', 'j','k', 'l', 'm', 'n', 'p', 'q','r',
+                's', 't', 'u', 'v', 'w', 'x', 'y', 'z'
+            });
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+
+namespace ROT1_Converter
+{
+    class Program
+    {
+        static Alphabets currentAlphabet;
+        
+        static char GetSymbol(char symbol, bool reverse = false)
+        {
+            var symbols = AlphabetData.GetAlphabetSymbols(currentAlphabet);
+            if (symbols == null)
+                return symbol;
+
+            var idx = symbols.IndexOf(char.ToLower(symbol));
+            if (idx < 0)
+                return symbol;
+            idx += reverse ? -1 : 1;
+
+            if (idx < 0)
+                idx += symbols.Count;
+            else if (idx > symbols.Count)
+                idx -= symbols.Count;
+
+            return char.IsLower(symbol) ? symbols[idx] : char.ToUpper(symbols[idx]);
+        }
+
+        static string Convert(string text, bool reverse = false)
+        {
+            string result = "";
+            foreach (var symbol in text)
+                result += GetSymbol(symbol, reverse);
+            return result;
+        }
+
+
+
+        static void Main()
+        {
+            void Restart(string text)
+            {
+                if (!string.IsNullOrEmpty(text))
+                    Console.WriteLine(text);
+                Console.Write("\nPress any key for restart...");
+                Console.ReadKey(true);
+            }
+
+        restart:
+            Console.Clear();
+
+            Console.Write("Select your alphabet : ");
+            currentAlphabet = AlphabetData.GetAlphabet(Console.ReadLine());
+
+            if (currentAlphabet == Alphabets.None)
+            {
+                Restart("Alphabet not found.");
+                goto restart;
+            }
+
+            Console.Write("Enter your type convertion '1/0' : ");
+
+            if (!uint.TryParse(Console.ReadLine(), out uint ui) || (ui < 0 || ui > 1))
+            {
+                Restart("Type not recognized.");
+                goto restart;
+            }
+
+            Console.Write("Enter your text : ");
+            Console.WriteLine($"Result : {Convert(Console.ReadLine(), ui == 0)}");
+
+            Console.Write("Press Enter key for exit..");
+            if (Console.ReadKey(true).Key != ConsoleKey.Enter)
+                goto restart;
+        }
+    }
+}
