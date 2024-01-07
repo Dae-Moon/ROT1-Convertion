e90f56f7d426436d148dda9c944677ee9b720548
Ётмй уь юуп рспшйубм уп уь дёк!!! <96235598+Dae-Moon@users.noreply.github.com>
2024-01-08 02:12:52 +0300

    Update Alphabets.cs
    Fix english alphabet

--- Alphabets.cs
+++ Alphabets.cs
@@ -67,7 +67,7 @@ namespace ROT1_Converter
         public static readonly ReadOnlyCollection<char> English = new ReadOnlyCollection<char>(new char[]
             {
                 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h',
-                'i', 'j','k', 'l', 'm', 'n', 'p', 'q','r',
+                'i', 'j','k', 'l', 'm', 'n', 'o', 'p', 'q','r',
                 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'
             });
     }
